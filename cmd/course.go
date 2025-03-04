451b4a129f42b5e33e32b2520d9a4c99c02f36d5
Rasmus Buurman <rasmus@rbuurman.de>
2025-03-04 18:36:07 +0100

    Implement uni course remove

--- cmd/course.go
+++ cmd/course.go
@@ -55,9 +55,41 @@ var addCourseCmd = &cobra.Command{
 	},
 }
 
+var deleteRemovedCourse bool
+var removeCourseCmd = &cobra.Command{
+	Use: "remove",
+	Aliases: []string { "rm" },
+	Short: "Remove a course",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+
+		if !config.ContainsCourse(name) {
+			fmt.Printf("The course %s does not exist.\n", name)
+			os.Exit(0)
+		}
+
+		delete(config.Courses, name)
+		config.WriteToFile(configFile)
+
+		if deleteRemovedCourse {
+			courseDirectory := filepath.Join(uniDirectory, name)
+			err := os.RemoveAll(courseDirectory)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error:", err)
+				os.Exit(1)
+			}
+		}
+
+		fmt.Printf("Success: Removed course %s.\n", name)
+	},
+}
+
 func init() {
 	addCourseCmd.Flags().StringVarP(&newFullName, "full-name", "f", "", "provide a more detailed name")
+	removeCourseCmd.Flags().BoolVarP(&deleteRemovedCourse, "delete", "D", false, "delete course directory")
 
 	courseCmd.AddCommand(listCoursesCmd)
 	courseCmd.AddCommand(addCourseCmd)
+	courseCmd.AddCommand(removeCourseCmd)
 }
