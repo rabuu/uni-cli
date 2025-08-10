16350a03200db372ca7af2e2c73d72f1eaf9f36b
Rasmus Buurman <rasmus@rbuurman.de>
2025-08-10 21:06:54 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "uni-cli - My in-house university workflow tool";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.just
+          ];
+        };
+      }
+    );
+}
